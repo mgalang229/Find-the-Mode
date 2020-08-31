040c331e29edfcb416ee77b4ce977e68fcba8131
Jason Galang <51401355+mgalang229@users.noreply.github.com>
2020-08-31 19:03:21 +0800

    Update sol.cpp

--- sol.cpp
+++ sol.cpp
@@ -9,16 +9,17 @@ int main() {
 	cin >> n;
 	for(int i=0; i<n; ++i)
 		cin >> a[i];
-	int cnt=1, md=0, tmp=0;
-	for(int i=1; i!=n+1; ++i) {
-		if(a[i-1]>a[i]) {
-			if(cnt>tmp) {
-				md=a[i-1];
-				tmp=cnt;
-				cnt=1;
-			}
-		} else
-			cnt++;
-	}
+	int max=*max_element(a, a+n);
+	int list[max+1];
+	for(int i=0; i<max+1; ++i)
+		list[i]=0;
+	for(int i=0; i<n; ++i)
+		list[a[i]]++;
+	int md=0, tmp=list[0];
+	for(int i=1; i<max+1; ++i)
+		if(list[i]>tmp) {
+			tmp=list[i];
+			md=i;
+		}
 	cout << md << "\n";
 }
